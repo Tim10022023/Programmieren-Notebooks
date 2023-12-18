38891682ca37d621af1d116f53b959739fe398a3
Tim Leute <tim.leute@wuerth-industrie.com>
2023-12-18 17:57:19 +0100

    added tree and turtle

---
+++ PythonProjects/turtle_.py
@@ -0,0 +1,18 @@
+import turtle
+
+ws = turtle.Turtle()
+ws.speed(0)
+
+def checkColor(number):
+   if number % 2 == 0:
+      ws.color("red")
+   else:
+      ws.color("orange")
+
+for j in range (1,100):
+  checkColor(j)
+  for i in range (1,6):
+      ws.left(190)
+      ws.forward(j*3)
+  ws.left(5)
+turtle.done()
