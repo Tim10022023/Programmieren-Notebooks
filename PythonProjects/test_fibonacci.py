4849af4cf5d90b9aed0fb542685bd43a97d1d2a2
Tim Leute <tim.leute@wuerth-industrie.com>
2024-02-05 09:58:45 +0100

    testing

---
+++ PythonProjects/test_fibonacci.py
@@ -0,0 +1,21 @@
+import pytest
+
+from fibonacci import fibonacci_of
+
+def test_fibonacci_of_zero():
+    assert fibonacci_of(0) == 0
+
+def test_fibonacci_of_one():
+    assert fibonacci_of(1) == 1
+
+def test_fibonacci_of_two():
+    assert fibonacci_of(2) == 1
+
+def test_fibonacci_of_three():
+    assert fibonacci_of(3) == 2
+
+def test_fibonacci_of_ten():
+    assert fibonacci_of(10) == 55
+
+# Wenn Ihre Funktion eine ungültige Eingabe überprüft, können Sie auch einen Test dafür schreiben
+
